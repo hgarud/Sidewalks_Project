8a6630159cea9ae1285e4c19512fe4ae322c987b
hgarud <hgarud@ncsu.edu>
2018-07-30 18:46:17 -0400

    added Unet

--- Model.py
+++ Model.py
@@ -156,7 +156,69 @@ class ResNet(object):
         
         return resnet
 
+class UNet(object):
+    
+    def __init__(self):        
+        print("Building the UNet architecture")
 
+    def CreateUNet(self, input_shape, n_labels=12):
+        input = Input(shape=input_shape)
+
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(input)
+        # print "conv1 shape:", conv1.shape
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+        # print "conv1 shape:", conv1.shape
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+        # print "pool1 shape:", pool1.shape
+
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        # print "conv2 shape:", conv2.shape
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+        # print "conv2 shape:", conv2.shape
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        # print "pool2 shape:", pool2.shape
+
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        # print "conv3 shape:", conv3.shape
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+        # print "conv3 shape:", conv3.shape
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+        # print "pool3 shape:", pool3.shape
+
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+        drop4 = Dropout(0.5)(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+        drop5 = Dropout(0.5)(conv5)
+
+        up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+        merge6 = merge([drop4, up6], mode='concat', concat_axis=3)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+        up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
+        merge7 = merge([conv3, up7], mode='concat', concat_axis=3)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+        up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
+        merge8 = merge([conv2, up8], mode='concat', concat_axis=3)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+        up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
+        merge9 = merge([conv1, up9], mode='concat', concat_axis=3)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv10 = Conv2D(n_labels, 1, activation='sigmoid')(conv9)
+
+        model = Model(input=input, output=conv10)
+
+        return model
         
 
 if __name__ == '__main__':
